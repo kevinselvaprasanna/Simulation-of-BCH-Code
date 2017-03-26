02cb3cc3a5862ce499931e12bf0a9263f49ff094
Kevin Selva Prasanna <kevinselvaprasanna@gmail.com>
2017-03-26 15:47:42 +0530

    BCH decoding over BSC channel

--- read_file.m
+++ read_file.m
@@ -1,5 +1,6 @@
 function data = read_file(file_name, k)
-file = fopen(file_name, 'r');
-data = fscanf(file, '%d');
-data = reshape(data, [length(data)/k k]);
-fclose(file);
+fileID = fopen(file_name,'r');
+sizeA = [k Inf];
+data = fscanf(fileID,'%d',sizeA);
+data =data';
+fclose(fileID);
